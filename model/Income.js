00826721b4c3fbfe6db985cf7c2e3174c09b5622
valent <valentinus.wastu@gmail.com>
2023-09-24 21:27:56 +0700

    add models

---
+++ model/Income.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const incomeSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  incomeName: {
+    type: String,
+    required: true,
+  },
+  incomeAmount: {
+    type: Number,
+    required: true,
+  },
+  incomeMonthly: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model('Income', incomeSchema);
