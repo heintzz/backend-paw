1bd63ef3b6cb150282338e3da4df0ad7bd1f9c52
heintzz <mhasnanr@gmail.com>
2023-09-25 10:07:02 +0700

    fix(model, controller): name importing

--- controller/income.controller.js
+++ controller/income.controller.js
@@ -1,97 +1,93 @@
-// Note: need integration with DB
-// const Income = require('../models/income');
+const Income = require("../model/income");
 
 // Create income record
 const createIncome = async (req, res) => {
-    try {
-      const { incomeName, incomeAmount, incomeMonthly } = req.body;
-      const userId = req.id; // Get the user ID from the authenticated user
-  
-      // Note : need integration with DB
-      // const income = new Income({
-      //   userId,
-      //   incomeName,
-      //   incomeAmount,
-      //   incomeMonthly
-      // });
-  
-      // await income.save();
-  
-      // res.status(201).json({ success: true, data: income });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
+  try {
+    // Get the user ID from the authenticated user
+    const userId = req.id;
+    const { incomeName, incomeAmount, incomeMonthly } = req.body;
+
+    const income = new Income({
+      userId,
+      incomeName,
+      incomeAmount,
+      incomeMonthly,
+    });
+
+    await income.save();
+
+    res.status(201).json({ success: true, data: income });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 };
 
 // Get income records for the user
 const getIncome = async (req, res) => {
-    try {
-        const userId = req.id; // Get the user ID from the authenticated user
-        
-        // Note: need integration with DB
-        // const income = await Income.find({userId});
-    
-        // if (!income) {
-        //     return res.status(404).json({ msg: 'Income record not found' });
-        // }
-    
-        // res.status(200).json({ success: true, data: income });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+  try {
+    // Get the user ID from the authenticated user
+    const userId = req.id;
+    const income = await Income.find({ userId });
+
+    if (!income) {
+      return res.status(404).json({ msg: "Income record not found" });
     }
+
+    res.status(200).json({ success: true, data: income });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 };
-  
+
 // Update income record
 const updateIncome = async (req, res) => {
-    try {
-        const incomeId = req.params.id;
-        const { incomeName, incomeAmount } = req.body;
+  try {
+    const incomeId = req.params.id;
+    const { incomeName, incomeAmount } = req.body;
 
-        // Note: need integration with DB
-        // const income = await Income.findOne({_id: incomeId});
+    const income = await Income.findOne({ _id: incomeId });
 
-        // if (!income) {
-        //     return res.status(404).json({ msg: 'Income record not found' });
-        // }
-
-        // income.incomeName = incomeName;
-        // income.incomeAmount = incomeAmount;
-
-        // await income.save();
-
-        // res.status(200).json({ success: true, data: income });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+    if (!income) {
+      return res.status(404).json({ msg: "Income record not found" });
     }
+
+    income.incomeName = incomeName;
+    income.incomeAmount = incomeAmount;
+
+    await income.save();
+
+    res.status(200).json({ success: true, data: income });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 };
 
 // Delete income record
 const deleteIncome = async (req, res) => {
-    try {
-        const incomeId = req.params.id; 
+  try {
+    const incomeId = req.params.id;
 
-        // Note: Need integration with DB
-        // const income = await Income.findOne({_id: incomeId});
+    const income = await Income.findOne({ _id: incomeId });
 
-        // if (!income) {
-        // return res.status(404).json({ msg: 'Income record not found' });
-        // }
-
-        // await income.remove();
-
-        // res.status(200).json({ success: true, data: 'Expense record removed' });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+    if (!income) {
+      return res.status(404).json({ msg: "Income record not found" });
     }
+
+    await income.deleteOne();
+
+    res.status(200).json({ success: true, data: "Expense record removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 };
 
 module.exports = {
-    createIncome,
-    getIncome,
-    updateIncome,
-    deleteIncome,
+  createIncome,
+  getIncome,
+  updateIncome,
+  deleteIncome,
 };
--- routes/income.js
+++ routes/income.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const verify = require('../middleware/verifyJWT');
-const incomeController = require('../controllers/income.controller');
+const verify = require("../middleware/verifyJWT");
+const incomeController = require("../controller/income.controller");
 
 // Create income record
-router.post('/', verify, incomeController.createIncome);
+router.post("/", verify, incomeController.createIncome);
 
 // Get income records for the user
-router.get('/', verify, incomeController.getIncome);
+router.get("/", verify, incomeController.getIncome);
 
 // Update income record
-router.put('/:id', verify, incomeController.updateIncome);
+router.put("/:id", verify, incomeController.updateIncome);
 
 // Delete income record
-router.delete('/:id', verify, incomeController.deleteIncome);
+router.delete("/:id", verify, incomeController.deleteIncome);
 
 module.exports = router;
