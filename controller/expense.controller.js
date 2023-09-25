a8d45da13bf6290ebb7cad8b326654d52488861a
heintzz <mhasnanr@gmail.com>
2023-09-25 10:39:33 +0700

    fix: update &  delete controller

--- controller/expense.controller.js
+++ controller/expense.controller.js
@@ -1,94 +1,91 @@
-// Note: need integration with DB
-// const Expense = require('../models/expense');
+const Expense = require("../model/expense");
 
 // Create expense record
 const createExpense = async (req, res) => {
-    try {
-        const { expenseName, expenseAmount, expenseCategory, expenseMonthly} = req.body;
-        const userId = req.id;
-        
-        // Note: need integration with DB
-        // const expense = new Expense({
-        //     userId,
-        //     expenseName,
-        //     expenseAmount,
-        //     expenseCategory,
-        //     expenseMonthly
-        // });
-    
-        // await expense.save();
-    
-        // res.status(201).json({ success: true, data: expense });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  };
+  try {
+    const userId = req.id;
+    const { expenseName, expenseAmount, expenseCategory, expenseMonthly } =
+      req.body;
+
+    const expense = new Expense({
+      userId,
+      expenseName,
+      expenseAmount,
+      expenseCategory,
+      expenseMonthly,
+    });
+
+    await expense.save();
+
+    res.status(201).json({ success: true, data: expense });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
 
 // Get expense records for the user
 const getExpense = async (req, res) => {
   try {
-    const userId = req.id; // Get the user ID from the authenticated user
+    // Get the user ID from the authenticated user
+    const userId = req.id;
 
-    const expense = await Expense.find({userId});
+    const expense = await Expense.find({ userId });
 
     res.status(200).json({ success: true, data: expense });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
 
 // Update expense record
 const updateExpense = async (req, res) => {
   try {
-    const expenseId = req.params.id; 
-    const { expenseName, expenseAmount, expenseCategory} = req.body;
+    const expenseId = req.params.id;
 
-    // Note: need integration with DB
-    // const expense = await Expense.findOne({ _id: expenseId});
+    const expense = await Expense.findOneAndUpdate(
+      { _id: expenseId },
+      req.body,
+      { new: true }
+    );
 
-    // if (!expense) {
-    //   return res.status(404).json({ msg: 'Expense record not found' });
-    // }
+    if (!expense) {
+      return res.status(404).json({ msg: "Expense record not found" });
+    }
 
-    // expense.expenseName = expenseName;
-    // expense.expenseAmount = expenseAmount;
-    // expense.expenseCategory = expenseCategory;
+    await expense.save();
 
-    // await expense.save();
-
-    // res.status(200).json({ success: true, data: expense });
+    res.status(200).json({ success: true, data: expense });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
 
 // Delete expense record
 const deleteExpense = async (req, res) => {
   try {
-    const expenseId = req.params.id; 
+    const expenseId = req.params.id;
 
-    // Note: need integration with DB
-    // const expense = await Expense.findOne({ _id: expenseId});
+    const expense = await Expense.findOne({ _id: expenseId });
 
-    // if (!expense) {
-    //   return res.status(404).json({ msg: 'Expense record not found' });
-    // }
+    if (!expense) {
+      return res.status(404).json({ msg: "Expense record not found" });
+    }
 
-    // await expense.remove();
+    await expense.deleteOne();
 
-    // res.status(200).json({ success: true, data: 'Expense record removed' });
+    res.status(200).json({ success: true, data: "Expense record removed" });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
 
 module.exports = {
-    createExpense,
-    getExpense,
-    updateExpense,
-    deleteExpense,
+  createExpense,
+  getExpense,
+  updateExpense,
+  deleteExpense,
 };
--- routes/expense.js
+++ routes/expense.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const verify = require('../middleware/verifyJWT');
-const expenseController = require('../controllers/expense.controller');
+const verify = require("../middleware/verifyJWT");
+const expenseController = require("../controller/expense.controller");
 
 // Create expense record
-router.post('/', verify, expenseController.createExpense);
+router.post("/", verify, expenseController.createExpense);
 
 // Get expense records for the user
-router.get('/', verify, expenseController.getExpense);
+router.get("/", verify, expenseController.getExpense);
 
 // Update expense record
-router.put('/:id', verify, expenseController.updateExpense);
+router.patch("/:id", verify, expenseController.updateExpense);
 
 // Delete expense record
-router.delete('/:id', verify, expenseController.deleteExpense);
+router.delete("/:id", verify, expenseController.deleteExpense);
 
 module.exports = router;
